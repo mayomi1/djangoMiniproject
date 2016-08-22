ba22cb5a711f81f507836989cb0fcbf64f9e0e94
Ayandiran Mayomi <ayandiranmayomi@gmail.com>
2016-08-22 02:31:27 +0100

    Add files via upload

---
+++ static/js/slide.js
@@ -0,0 +1,21 @@
+  $(document).ready(function(){
+      $('.slide').slick({
+        setting-name: setting-value
+         dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+      });
+    });
+
+
+
+
+  $('.jcarousel').jcarousel({
+    list: '.jcarousel-list'
+});
